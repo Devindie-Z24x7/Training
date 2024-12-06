afe11d90164c231fb93a639855d366e88c41a9ae
Devindies <devindies@zone24x7.com>
2024-12-06 08:56:11 +0530

    Created the calculator with error handling

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter the first number ");
+            double number1 = scanner.nextDouble();
+
+            System.out.print("Enter the second number ");
+            double number2 = scanner.nextDouble();
+
+            System.out.print("Select the operation you want to perform \n");
+            System.out.print("Addition : 1 \n");
+            System.out.print("Subtraction : 2 \n");
+            System.out.print("Multiplication : 3 \n");
+            System.out.print("Division : 4 \n");
+
+            int operation = scanner.nextInt();
+            double answer = switch (operation) {
+                case 1 -> calculator.addition(number1, number2);
+                case 2 -> calculator.subtraction(number1, number2);
+                case 3 -> calculator.multiplication(number1, number2);
+                case 4 -> calculator.division(number1, number2);
+                default -> throw new IllegalArgumentException("Invalid Operation");
+            };
+
+            System.out.println("The answer is: " + answer);
+        }
+        catch(ArithmeticException | IllegalArgumentException e){
+            System.out.println("Error : "+ e.getMessage());
+        }
+        catch(Exception e){
+            System.out.println("Error : Invalid Input ");
+        }
+
+
+
+    }
+}
